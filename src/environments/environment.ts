33f826ed3d2fd1b89dd8f62b8d013c559dc10fab
milanbukvic <milanbukvic2017@gmail.com>
2018-09-17 00:30:50 +0200

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig : {
+      apiKey: "AIzaSyBKDduMCNe8CkX4LBxq_mdmumZcjdteCHA",
+      authDomain: "fir-crudapp-df640.firebaseapp.com",
+      databaseURL: "https://fir-crudapp-df640.firebaseio.com",
+      projectId: "fir-crudapp-df640",
+      storageBucket: "fir-crudapp-df640.appspot.com",
+      messagingSenderId: "878448369171"
+    }
+};
